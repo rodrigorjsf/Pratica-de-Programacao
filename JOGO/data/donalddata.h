1414c63f9fdb8074d82ea369e03f0c0574dd2da4
Rodrigo França <rodrigo_rjsf@hotmail.com>
2017-06-01 03:48:12 -0300

    Add files via upload

---
+++ JOGO/data/donalddata.h
@@ -0,0 +1,18 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.0, MSVC.s */
+/* Datafile: c:\Users\Rodrigo\workspace\RACHA CUCA\Debug\donald.dat */
+/* Date: Thu Jun 01 02:00:15 2017 */
+/* Do not hand edit! */
+
+#define D_TRES_CINCO                     0        /* BMP  */
+#define D_TRES_DOIS                      1        /* BMP  */
+#define D_TRES_OITO                      2        /* BMP  */
+#define D_TRES_QUATRO                    3        /* BMP  */
+#define D_TRES_SEIS                      4        /* BMP  */
+#define D_TRES_SETE                      5        /* BMP  */
+#define D_TRES_TRES                      6        /* BMP  */
+#define D_TRES_UM                        7        /* BMP  */
+#define DONALD                           8        /* BMP  */
+#define DONALDMEDIOCINCO                 9        /* BMP  */
+#define DONALDMEDIOQUATRO                10       /* BMP  */
+#define DONALDMEDIOTRES                  11       /* BMP  */
+
---
+++ JOGO/data/soundfile.h
@@ -0,0 +1,10 @@
+/* Allegro datafile object indexes, produced by grabber v4.2.0, MSVC.s */
+/* Datafile: c:\Users\Rodrigo\workspace\Jogo\Debug\sons.dat */
+/* Date: Wed May 31 18:45:11 2017 */
+/* Do not hand edit! */
+
+#define CLICK                            0        /* SAMP */
+#define MUSICA                           1        /* MIDI */
+#define RECORD                           2        /* SAMP */
+#define VITORIA                          3        /* SAMP */
+
