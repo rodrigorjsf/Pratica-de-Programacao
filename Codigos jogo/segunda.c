d15800b9d6252fbe1de3a5abcb4c80680f457eda
Rodrigo França <rodrigo_rjsf@hotmail.com>
2017-06-02 16:09:22 -0300

    Update segunda.c

--- Codigos jogo/segunda.c
+++ Codigos jogo/segunda.c
@@ -60,8 +60,7 @@ int segunda_tela(){
 		{
 			draw_sprite(bufferAux, mickey, 110, 250);
 			if(mouse_b == 1){
-				mouse_b = 0;
-				textout_ex(bufferAux, font, "TESTE", 100, 100, makecol(0, 0, 255), -1);
+				mouse_b = 0;				
 				op = 0;
 				if(tela_dificuldade(op) == 1)
 					sair_segunda_tela = TRUE;
@@ -72,8 +71,7 @@ int segunda_tela(){
 		{
 			draw_sprite(bufferAux, donald, 510, 250);
 			if(mouse_b == 1){
-				mouse_b = 0;
-				textout_ex(bufferAux, font, "TESTE", 100, 100, makecol(0, 0, 255), -1);
+				mouse_b = 0;				
 				op = 1;
 				if(tela_dificuldade(op) == 1)
 					sair_segunda_tela = TRUE;
